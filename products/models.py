13d8a7506377ac7d3fe7548fe4d68a48c69e1557
Anjalizi <aj2966@gmail.com>
2020-05-31 17:03:32 +0530

    add code for blog 1.3

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	cost = models.IntegerField()
+	dimension = models.DecimalField(max_digits=10, decimal_places=2)
+
+	def __str__(self):
+		return "Dimension (in inches): " + str(self.dimension)
